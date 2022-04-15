e3d3ae6a31329119eb75a7e197bfa9b6397ca525
Miguel Angel Ramos <return.infox@gmail.com>
2022-04-14 22:32:30 -0400

    Firebase

---
+++ src/components/files/FilesList.vue
@@ -0,0 +1,60 @@
+<template>
+  <div class="files-list">
+    <p class="file-list__not-found">
+      No existen archivos en la base datos
+    </p>
+
+    <div class="files-list__filesdb" v-for="file of filesList" :key="file.id">
+      <p>{{ file.dateString }}</p>
+      <!-- <p>info</p> -->
+      <div class="action">
+        <!-- borrar
+        descargar -->
+      </div>
+    </div>
+
+
+  </div>
+
+</template>
+
+<script>
+import moment from "moment";
+import {db, auth} from "@/utils/firebase"; // firebase
+export default {
+  name: 'FileList',
+  props: {
+    filesList: Array
+  },
+  setup() {
+    const formatDate = date  => moment(date).format('D [de] MMMM YYYY');
+
+    return {
+      formatDate
+    }
+    
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .files-list {
+    &__not-found {
+      text-align: center;
+      font-size: 20px;
+    }
+
+    &__filesdb {
+      display: flex;
+      align-items: center;
+      justify-content: space-between;
+      margin: 15px 0;
+      p {
+        margin: 0;
+        text-transform: uppercase;
+        font-weight: bold;
+      }
+    }
+
+  }
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -6,24 +6,54 @@
       <!-- Necesitamos componente para cargar un archivos -->
       <FileUpload />
       <!-- Mostrar los archivos -->
-      
+      <FilesList :filesList="showFiles"/>
+    
     </div>
   </Layout>
 </template>
 
 <script>
 // @ is an alias to /src
-
+import { ref } from 'vue';
+import { onMounted } from "vue";
 import Layout from "../layout/Layout.vue";
 import FileUpload from "@/components/files/FileUpload.vue";
+import FilesList from "@/components/files/FilesList.vue";
+import {db, auth} from "@/utils/firebase"; // firebase
 export default {
   name: 'HomeView',
   components: {
     Layout,
-    FileUpload
+    FileUpload,
+    FilesList
   },
   setup() {
+    const showFiles = ref(null);
+    onMounted( () => {
+      // llamar una funcion que obtenga la informacion desde BD
+      getFiles();
+    });
+
+    // FUNCION QUE TRAE LA INFO
+    const getFiles = async () => {
+      const response = await db.collection(auth.currentUser.uid).orderBy("date", "desc").get();
+      // console.log(response)
+      const result = [];
+
+      response.docs.forEach( doc => {
+        console.log(typeof doc.data()); // es un objeto
+        console.log(doc.id);
+
+        // un objeto llamado data
+        const data = doc.data();
+        data.id = doc.id;
+        result.push(data);
+      })
+      console.log(result);
+      showFiles.value = result; // arreglo
+    }
     return {
+      showFiles
     }
   }
 }
