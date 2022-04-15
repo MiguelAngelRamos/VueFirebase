7a3bc54398531053913f420f595298575304e18a
Miguel Angel Ramos <return.infox@gmail.com>
2022-04-14 21:29:48 -0400

    formulario

--- src/components/files/FileUpload.vue
+++ src/components/files/FileUpload.vue
@@ -1,8 +1,12 @@
 <template>
   <div class="upload-file">
-    <button class="ui button primary fluid">Nuevo Archivo</button>
+    <button class="ui button primary fluid" @click="showCloseOpenForm">Nuevo Archivo</button>
 
-    <form class="ui form upload-file__form" @submit.prevent="handleSubmit">
+    <form 
+    class="ui form upload-file__form" 
+    @submit.prevent="handleSubmit"
+    :class="{open: showForm}"
+    >
 
       <div class="field">
         <label class="ui button" for="file">
@@ -20,8 +24,8 @@
         </div>
       </div>
 
-      <button class="ui button positive fluid" type="submit">Subir archivo</button>
-
+      <button class="ui button positive fluid" type="submit" :class="{loading}">Subir archivo</button>
+      <p v-if="error">{{error}}</p>
     </form>
   </div>
 </template>
@@ -34,14 +38,21 @@ import { auth, storage, db } from "@/utils/firebase";
 export default {
   name: 'FileUpload',
   setup() {
+    let showForm = ref(false);
     const loading = ref(false);
     const error = ref(null);
     const file = ref(null);
     const date = ref(null);
 
+    // mostrar o ocultar el formulario
+    const showCloseOpenForm = () => {
+      console.log('llamando')
+      showForm.value = !showForm.value;
+    }
     const uploadFile = (event) => {
-      console.log("Eventos del uploadFile");
-      console.log(event.target.files[0]);
+      error.value = null;
+      // console.log("Eventos del uploadFile");
+      // console.log(event.target.files[0]);
       let fileTemp = event.target.files[0];
       // type: "application/pdf"
       if(fileTemp?.type === "application/pdf") {
@@ -77,13 +88,21 @@ export default {
           // Obtener la url del fichero
           const fileUrl = await storage.ref(`${auth.currentUser.uid}/${nameFile}.pdf`).getDownloadURL();
           console.log(fileUrl);
-          // Subir el archivo al storage
-          
+          // Crear un documento en la base de datos
+          await db.collection(auth.currentUser.uid).add({
+            fileUrl: fileUrl,
+            date: new Date(date.value), // va estar en formato de javascript
+            dateString: date.value
+          })
 
         } catch (error) {
-          
+          console.log(error);
         }
-        // console.log('enviando archivo a la base de datos...');
+        // el archivo ya se subio a la base de datos
+        loading.value = false;
+        // resetear el formulario
+        file.value = null;
+        date.value = null;
       } else {
         console.log('Suba un archivo pdf y seleccione la fecha');
       }
@@ -92,12 +111,30 @@ export default {
     return {
       uploadFile,
       changeDate,
-      handleSubmit
+      handleSubmit,
+      error,
+      showCloseOpenForm,
+      showForm
     }
   }
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+  .upload-file {
+    &__form {
+      border: 1px solid #d4d4d4;
+      border-radius: 10px;
+      height: 0px;
+      overflow: hidden;
+      transition: 0.5s ease;
+
+      &.open{
+        height: 200px;
+        margin-top: 20px;
+        padding: 20px;
+      }
+    }
+  }
 
 </style>
